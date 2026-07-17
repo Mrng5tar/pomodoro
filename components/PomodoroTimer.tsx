714b833e7519f5aa65909ef401162fbbfe38dc7d
Mrng5tar <ashutoshgupta1825@gmail.com>
2026-07-18 05:19:27 +0530

    feat: add new UI components and utility functions, walk timer, toggle image
    - Introduced a new Label component for form labeling.
    - Added a Separator component for visual separation in UI.
    - Created a Switch component for toggle functionality.
    - Implemented a utility function `cn` for conditional class names using clsx and tailwind-merge.
    - Added audio files in Ogg format for media handling.

--- components/PomodoroTimer.tsx
+++ components/PomodoroTimer.tsx
@@ -2,9 +2,13 @@
 
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
+import { Switch } from "./ui/switch";
+import { Field, FieldContent, FieldDescription, FieldLabel } from "./ui/field";
 
 const WORK_TIME = 20 * 60; // 20 minutes
 const BREAK_TIME = 30;     // 30 seconds
+
+const audios = ["/1.ogg", "/2.ogg", "/3.ogg"]
 
 type Phase = "work" | "break";
 
@@ -12,14 +16,25 @@ export default function PomodoroTimer() {
   const [running, setRunning] = useState(false);
   const [remaining, setRemaining] = useState(WORK_TIME);
   const [phase, setPhase] = useState<Phase>("work");
+  const [showImage, setShowImage] = useState(true)
+  const cardRef = useRef(null)
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const walkAudioRef = useRef<HTMLAudioElement | null>(null);
+
+  if (!sessionStorage.getItem("session-start-time")) {
+  sessionStorage.setItem(
+    "session-start-time",
+    Date.now().toString()
+  );
+}
 
   // Load saved session
   useEffect(() => {
     const isRunning = localStorage.getItem("pomodoro-running") === "true";
     const startTime = localStorage.getItem("pomodoro-start-time");
     const savedPhase = localStorage.getItem("pomodoro-phase") as Phase;
+
 
     if (isRunning && startTime && savedPhase) {
       setRunning(true);
@@ -30,6 +45,7 @@ export default function PomodoroTimer() {
 
   // Timer loop
   useEffect(() => {
+
     if (!running) return;
 
     const interval = setInterval(() => {
@@ -43,6 +59,32 @@ export default function PomodoroTimer() {
 
     return () => clearInterval(interval);
   }, [running]);
+
+  //walk time
+  useEffect(() => {
+
+    if (!running) return;
+
+    const interval = setInterval(() => {
+      const startTime = Number(
+        sessionStorage.getItem("session-start-time")
+      );
+    if(Date.now() - startTime >= 1000 * 60 *60){
+      sessionStorage.setItem("session-start-time", Date.now().toString())
+
+      if (walkAudioRef.current) {
+      walkAudioRef.current.src = getRandomAudio()
+      walkAudioRef.current.currentTime = 0;
+      walkAudioRef.current.play();
+    }
+    }
+
+      
+    }, 1000 * 60);
+
+    return () => clearInterval(interval);
+  }, [running]);
+
 
   function calculateRemaining(startTime: number, currentPhase: Phase) {
     const now = Date.now();
@@ -128,16 +170,35 @@ export default function PomodoroTimer() {
     }
   }
 
+  function getRandomAudio(){
+    
+
+    const random = audios[Math.floor(Math.random() * audios.length)]
+
+    return random
+  }
+
+  function handleToggle(e:React.MouseEvent){
+    if (e.target === cardRef.current) {
+      setShowImage(prev => !prev)
+    }
+  }
+
+
   const minutes = Math.floor(remaining / 60);
   const seconds = remaining % 60;
 
+
   return (
-    <div className="w-[380px] p-10 rounded-2xl shadow-xl text-center bg-black/50">
+    <div className="w-[380px] p-10 rounded-2xl shadow-xl text-center bg-black/50" ref={cardRef} onClick={(e)=>{
+      handleToggle(e)
+    }}>
+      
 
-      <div className="bg-white w-screen h-screen absolute top-0 left-0 -z-40">
-        {!running && (<Image src={"/home.png"} width={1920} height={1080} className="object-cover w-full h-full" alt="home image"/>)}
+      <div className={`${showImage ? "bg-white" : "bg-neutral-800"} w-screen h-screen absolute top-0 left-0 -z-40`}>
+        {!running && showImage && (<Image src={"/home.png"} width={1920} height={1080} className="object-cover w-full h-full" alt="home image"/>)}
 
-        {running && phase === "work" && (<Image src={"/work.png"} width={1920} height={1080} className="object-cover w-full h-full" alt="work image"/>)}
+        {running && showImage && phase === "work" && (<Image src={"/work.png"} width={1920} height={1080} className="object-cover w-full h-full" alt="work image"/>)}
 
         {running && phase === "break" && (<Image src={"/break.png"} width={1920} height={1080} className="object-cover w-full h-full" alt="work image"/>)}
       </div>
@@ -170,7 +231,13 @@ export default function PomodoroTimer() {
       {phase === "work" && running && <button onClick={switchToBreak}
           className="bg-green-800 hover:bg-green-900 text-white px-6 py-3 rounded-lg w-full hover:cursor-pointer mt-4">Take a break</button>}
 
+          
+      
+  
+
       <audio ref={audioRef} src="/one_piece.mp3" preload="auto" />
+
+      <audio ref={walkAudioRef} src="/1.ogg" preload="auto" />
     </div>
   );
 }
